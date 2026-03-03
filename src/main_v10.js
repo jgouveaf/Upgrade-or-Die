08965a7e3058c4e08aa7eb763266ebc02de4c78b
jgouveaf <joaogouvea10@gmail.com>
2026-03-03 17:15:01 -0300

    Morcegos

---
+++ src/main_v10.js
@@ -0,0 +1,26 @@
+import { StartScene } from './scenes/StartScene.js';
+import { GameScene } from './scenes/GameScene.js';
+import { UpgradeScene } from './scenes/UpgradeScene.js';
+import { GameOverScene } from './scenes/GameOverScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    backgroundColor: '#0a0a0c',
+    render: {
+        pixelArt: true,
+        antialias: false,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [StartScene, GameScene, UpgradeScene, GameOverScene]
+};
+
+const game = new Phaser.Game(config);
